226cffe1306c01b6efda2718a5f968fd75cf01d3
ramon cantanhede gusmão <ramoncgusmao@gmail.com>
2020-01-24 01:19:55 -0300

    resolvendo o problema do foreach usando um map e promisse

--- src/controllers/NotaDeCorteController.js
+++ src/controllers/NotaDeCorteController.js
@@ -8,25 +8,29 @@ module.exports = {
         const codigoCursoArray = codigocurso.split(',').map(texto => texto.trim());
         let lista = new Array();
 
-        codigoCursoArray.forEach( async codigo => {
+        const result = codigoCursoArray.map( async codigo  => {
 
         const requisicao = await axios.get(`https://sisu-api.apps.mec.gov.br/api/v1/oferta/${codigo}/modalidades`);
     
+
         const { oferta, modalidades } = requisicao.data;
 
         let notadecorte = 
             {
-            'campos': oferta.no_campus,
-            'siglafaculdade': oferta.sg_ies,
+            'campus': oferta.no_campus,
+            'siglaFaculdade': oferta.sg_ies,
             'curso': oferta.no_curso,
             'turno': oferta.no_turno,
-            'nota': modalidades.find(no_concorrencia => no_concorrencia = "Ampla concorrência").nu_nota_corte,
-            "siglaTipoVaga": "A0"
+            'notaDeCorte': modalidades.find(no_concorrencia => no_concorrencia = "Ampla concorrência").nu_nota_corte,
+            'siglaTipoVaga': 'A0',
+            'dia': 3
             };
         console.log(notadecorte);
         lista.push(notadecorte);
        
     }); 
+
+    await Promise.all(result)
     
         return response.json(lista);
     } 
